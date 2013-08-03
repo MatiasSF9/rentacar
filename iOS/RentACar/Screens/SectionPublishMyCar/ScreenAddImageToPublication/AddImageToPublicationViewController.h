01f138b57d49dc9e705c885ac8ab8cedd4041d95
Matiasrf <matt@Garines-MacBook.local>
2013-08-03 10:37:58 -0300

    ADD: Image Picker

--- iOS/RentACar/Screens/SectionPublishMyCar/ScreenAddImageToPublication/AddImageToPublicationViewController.h
+++ iOS/RentACar/Screens/SectionPublishMyCar/ScreenAddImageToPublication/AddImageToPublicationViewController.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AddImageToPublicationViewController : UIViewController
+@interface AddImageToPublicationViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UIButton *btnTakePicture;
+@property (strong, nonatomic) IBOutlet UIImageView *imgTakenPicture;
 
 @end
--- iOS/RentACar/Screens/SectionPublishMyCar/ScreenAddImageToPublication/AddImageToPublicationViewController.m
+++ iOS/RentACar/Screens/SectionPublishMyCar/ScreenAddImageToPublication/AddImageToPublicationViewController.m
@@ -14,6 +14,8 @@
 
 @implementation AddImageToPublicationViewController
 
+#pragma mark - Lifecycle
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -26,13 +28,53 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Image Management
+
+- (IBAction)takePicture:(UIButton *)sender {
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        
+        [[[UIAlertView alloc] initWithTitle:@""message:@"This device does not support camera pictures" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil] show];
+        return;
+    }
+
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.sourceType = (sender == self.btnTakePicture) ? UIImagePickerControllerSourceTypeCamera : UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    [self presentModalViewController:picker animated:YES];
+    
+}
+
+/*
+ *Opens library to pick images
+ */
+- (IBAction)selectLibraryPicture:(UIButton *)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        picker.delegate = self;
+        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        [self presentModalViewController:picker animated:YES];
+    }
+    else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error accessing photo library" message:@"Device does not support a photo library" delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo {
+    self.imgTakenPicture.image = image;
+    [picker dismissModalViewControllerAnimated:YES];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissModalViewControllerAnimated:YES];
 }
 
 @end
