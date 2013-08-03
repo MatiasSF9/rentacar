617e88092e4ef9fe8f1431bcadb405b5a125c1e4
Matias Paillet <matias.paillet@vmbc.com>
2013-08-03 13:41:05 -0300

    Added test cases for PublicationBuilder

--- iOS/RentACarTests/RentACarTests.m
+++ iOS/RentACarTests/RentACarTests.m
@@ -7,26 +7,48 @@
 //
 
 #import "RentACarTests.h"
+#import "PublicationBuilder.h"
+#import <CoreLocation/CoreLocation.h>
 
 @implementation RentACarTests
 
 - (void)setUp
 {
     [super setUp];
+    //Here goes all setup code for tests.
     
-    // Set-up code here.
+    //Makes the singleton initialize
+    [PublicationBuilder sharedInstance];
 }
 
 - (void)tearDown
 {
-    // Tear-down code here.
+    // Tear-down code here. Nothing needs to be done
     
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testBuilderAddImageSuccesfullyAdded
 {
-    STFail(@"Unit tests are not implemented yet in RentACarTests");
+    CLLocationCoordinate2D location = [[[CLLocation alloc] initWithLatitude:-43.002322 longitude:-43.21212] coordinate];
+    STAssertTrue([[PublicationBuilder sharedInstance] addImageToPublicationWithTitle:@"aTitle"
+                                                                         description:@"aDescription" inLocation:location withImageRawData:[[NSData alloc] init]],  @"testBuilderAddImageSuccesfullyAdded FAILED");
+}
+
+- (void)testBuilderAddImageSuccesfullyWithoutDescription
+{
+    CLLocationCoordinate2D location = [[[CLLocation alloc] initWithLatitude:-43.002322 longitude:-43.21212] coordinate];
+    STAssertTrue([[PublicationBuilder sharedInstance] addImageToPublicationWithTitle:@"aTitle"
+                                                                         description:nil
+                                                                         inLocation:location withImageRawData:[[NSData alloc] init]],  @"testBuilderAddImageSuccesfullyWithoutDescription FAILED");
+}
+
+- (void)testBuilderAddImageFailureForImageMissing
+{
+    CLLocationCoordinate2D location = [[[CLLocation alloc] initWithLatitude:-43.002322 longitude:-43.21212] coordinate];
+    STAssertFalse([[PublicationBuilder sharedInstance] addImageToPublicationWithTitle:@"aTitle"
+                                                                         description:@"aDescription"
+                                                                          inLocation:location withImageRawData:nil],@"testBuilderAddImageFailureForImageMissing FAILED");
 }
 
 @end
